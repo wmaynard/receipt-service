3548e1744baa4e0f69b248a931d5584f81990f71
Nathan Mac <nathan.mac@rumbleentertainment.com>
2021-12-02 15:50:52 -0800

    validation models refactor to accomodate google validation

--- Models/AppleValidation.cs
+++ Models/AppleValidation.cs
@@ -1,10 +1,65 @@
+using System.Text.Json.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Rumble.Platform.ReceiptService.Models
 {
     public class AppleValidation : Validation
     {
-        public AppleValidation(string status, Receipt response, string transactionId, string offerId, string receiptKey, string receiptData, long timestamp) 
-            : base(status: status, response: response, transactionId: transactionId, offerId: offerId, receiptKey: receiptKey, receiptData: receiptData, timestamp: timestamp)
-        { }
+        internal const string DB_KEY_STATUS = "status";
+        internal const string DB_KEY_RESPONSE = "resp";
+        internal const string DB_KEY_TRANSACTION_ID = "tid";
+        internal const string DB_KEY_OFFER_ID = "oid";
+        internal const string DB_KEY_RECEIPT_KEY = "rcptKey";
+        internal const string DB_KEY_RECEIPT_DATA = "rcptData";
+        internal const string DB_KEY_TIMESTAMP = "tmestmp";
+
+        public const string FRIENDLY_KEY_STATUS = "status";
+        public const string FRIENDLY_KEY_RESPONSE = "response";
+        public const string FRIENDLY_KEY_TRANSACTION_ID = "transactionId";
+        public const string FRIENDLY_KEY_OFFER_ID = "offerId";
+        public const string FRIENDLY_KEY_RECEIPT_KEY = "receiptKey";
+        public const string FRIENDLY_KEY_RECEIPT_DATA = "receiptData";
+        public const string FRIENDLY_KEY_TIMESTAMP = "timestamp";
+        
+        [BsonElement(DB_KEY_STATUS)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_STATUS)]
+        public string Status { get; private set; }
+        
+        [BsonElement(DB_KEY_RESPONSE)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_RESPONSE)]
+        public Receipt Response { get; private set; }
+
+        [BsonElement(DB_KEY_TRANSACTION_ID)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_TRANSACTION_ID)]
+        public string TransactionId { get; private set; }
+
+        [BsonElement(DB_KEY_OFFER_ID)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_OFFER_ID)]
+        public string OfferId { get; private set; }
+
+        [BsonElement(DB_KEY_RECEIPT_KEY)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_RECEIPT_KEY)]
+        public string ReceiptKey { get; private set; }
+
+        [BsonElement(DB_KEY_RECEIPT_DATA)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_RECEIPT_DATA)]
+        public string ReceiptData { get; private set; }
+
+        [BsonElement(DB_KEY_TIMESTAMP)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_TIMESTAMP)]
+        public long Timestamp { get; private set; }
+
+        public AppleValidation(string status, Receipt response, string transactionId, string offerId, string receiptKey,
+            string receiptData, long timestamp)
+        {
+            Status = status;
+            Response = response;
+            TransactionId = transactionId;
+            OfferId = offerId;
+            ReceiptKey = receiptKey;
+            ReceiptData = receiptData;
+            Timestamp = timestamp;
+        }
     }
 }
 
--- Models/GoogleValidation.cs
+++ Models/GoogleValidation.cs
@@ -1,11 +1,67 @@
+using System.Text.Json.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Rumble.Platform.ReceiptService.Models
 {
     public class GoogleValidation : Validation
     {
-        public GoogleValidation(string status, Receipt response, string transactionId, string offerId, string receiptKey, string receiptData, long timestamp)
-            : base(status: status, response: response, transactionId: transactionId, offerId: offerId, receiptKey: receiptKey, receiptData: receiptData, timestamp: timestamp)
+        /*
+        will require -only- the following:
+        "orderId": "your.order.id",
+        "packageName": "your.package.name",
+        "productId": "your.product.id",
+        "purchaseTime": 1526476218113,
+        "purchaseState": 0,
+        "purchaseToken": "your.purchase.token"
+        */
+
+        internal const string DB_KEY_ORDER_ID = "oid";
+        internal const string DB_KEY_PACKAGE_NAME = "pkgNm";
+        internal const string DB_KEY_PRODUCT_ID = "prodId";
+        internal const string DB_KEY_PURCHASE_TIME = "purchTime";
+        internal const string DB_KEY_PURCHASE_STATE = "purchState";
+        internal const string DB_KEY_PURCHASE_TOKEN = "purchTkn";
+
+        public const string FRIENDLY_KEY_ORDER_ID = "orderID";
+        public const string FRIENDLY_KEY_PACKAGE_NAME = "packageName";
+        public const string FRIENDLY_KEY_PRODUCT_ID = "productId";
+        public const string FRIENDLY_KEY_PURCHASE_TIME = "purchaseTime";
+        public const string FRIENDLY_KEY_PURCHASE_STATE = "purchaseState";
+        public const string FRIENDLY_KEY_PURCHASE_TOKEN = "purchaseToken";
+        
+        [BsonElement(DB_KEY_ORDER_ID)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_ORDER_ID)]
+        public string OrderId { get; private set; }
+
+        [BsonElement(DB_KEY_PACKAGE_NAME)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_PACKAGE_NAME)]
+        public string PackageName { get; private set; }
+        
+        [BsonElement(DB_KEY_PRODUCT_ID)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_PRODUCT_ID)]
+        public string ProductId { get; private set; }
+        
+        [BsonElement(DB_KEY_PURCHASE_TIME)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_PURCHASE_TIME)]
+        public long PurchaseTime { get; private set; }
+        
+        [BsonElement(DB_KEY_PURCHASE_STATE)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_PURCHASE_STATE)]
+        public int PurchaseState { get; private set; }
+        
+        [BsonElement(DB_KEY_PURCHASE_TOKEN)]
+        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_PURCHASE_TOKEN)]
+        public string PurchaseToken { get; private set; }
+
+        public GoogleValidation(string orderId, string packageName, string productId, long purchaseTime,
+            int purchaseState, string purchaseToken)
         {
-            
+            OrderId = orderId;
+            PackageName = packageName;
+            ProductId = productId;
+            PurchaseTime = purchaseTime;
+            PurchaseState = purchaseState;
+            PurchaseToken = purchaseToken;
         }
     }
 }
