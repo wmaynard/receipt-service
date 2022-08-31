579235edffee7f63747ee96ee58d8d4eba98d47e
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-08-31 14:34:12 -0700

    google validation apparently works, cleanup

--- Services/GoogleService.cs
+++ Services/GoogleService.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Security.Cryptography;
-using System.Text;
-using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Security;
 using RCL.Logging;
 using Rumble.Platform.Common.Utilities;
 using Rumble.Platform.ReceiptService.Exceptions;
@@ -19,79 +15,13 @@ public class GoogleService : VerificationService
         VerificationResult verification = null;
         
         if (signature == null)
-            throw new ReceiptException(receipt, "Failed to verify Google receipt. No signature.");
-        
-        // TODO
-        // need a valid receipt to test, not sure if the one on documentation is outdated
-        // androidStoreKey is a RSA public key. private key not provided by google
-        // maybe eventually make actual certificates to store
-        
-        // need to match exactly for verification, use googlevalidation for this purpose
+            throw new ReceiptException(receipt, "Failed to verify Google receipt. No signature provided.");
 
-        // TODO: Is this model even needed?
-        // This is the only place that it seems to appear, and all of the values are identical.
-        GoogleValidation purchaseInfo = new GoogleValidation
-        {
-            OrderId = receipt.OrderId,
-            PackageName = receipt.PackageName,
-            ProductId = receipt.ProductId,
-            PurchaseTime = receipt.PurchaseTime,
-            PurchaseState = receipt.PurchaseState,
-            PurchaseToken = receipt.PurchaseToken,
-            Acknowledged = receipt.Acknowledged
-        };
-        
-        /*
-        byte[] purchaseInfoBytes = Encoding.UTF8.GetBytes(purchaseInfo.JSON);
-        // byte[] purchaseInfoBytes = Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF8.GetBytes(purchaseInfo.JSON)));
-        
-        byte[] sigBytes = Encoding.UTF8.GetBytes(signature);
-        // byte[] sigBytes = Convert.FromBase64String(signature);
-        
-        byte[] keyBytes = Encoding.UTF8.GetBytes(PlatformEnvironment.Require(key: "androidStoreKey"));
-        // byte[] keyBytes = Convert.FromBase64String(PlatformEnvironment.Require(key: "androidStoreKey"));
-        
-        // AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
-        // RsaKeyParameters rsaKeyParameters = (RsaKeyParameters) asymmetricKeyParameter;
-        
-        RSAParameters rsaParameters = new RSAParameters();
-
-        // rsaParameters.Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned();
-        // rsaParameters.Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned();
-
-        byte[] modulus = new byte[256];
-        for (int i = 0; i < 256; i++)
-        {
-            modulus[i] = keyBytes[33 + i];
-        }
-
-        byte[] exponent = new byte[3];
-        for (int i = 0; i < 3; i++)
-        {
-            exponent[i] = keyBytes[291 + i];
-        }
-
-        rsaParameters.Modulus = modulus;
-        rsaParameters.Exponent = exponent;
-        
-        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
-        rsa.ImportParameters(rsaParameters);
-
-        SHA1 sha1 = SHA1.Create();
-        byte[] hash = sha1.ComputeHash(purchaseInfoBytes);
-        
-        RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
-        rsaDeformatter.SetHashAlgorithm(strName: "SHA1");
-        */
-        
         bool verified;
 
         try
         {
-            // TODO try another receipt with a tested valid signature
-            // the following appears to return false for the provided sample data sent
-            // verified = rsaDeformatter.VerifySignature(rgbHash: hash, rgbSignature: sigBytes);
-            
+            // androidStoreKey is a RSA public key. private key not provided by google
             GoogleSignatureVerify signatureVerify =
                 new GoogleSignatureVerify(PlatformEnvironment.Require(key: "androidStoreKey"));
             verified = signatureVerify.Verify(receipt.JSON.Replace(",\"accountId\":null,\"id\":null", ""), signature);
